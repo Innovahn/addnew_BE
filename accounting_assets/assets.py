1eec56580c36c1432039d831324e54ffbf0c9ef3
sgarcia@innovahn.com <sgarcia@innovahn.com>
2014-11-04 11:47:20 -0600

    Agregado nuevos addons de BE

---
+++ accounting_assets/assets.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+
+import time
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
+from openerp.tools.translate import _
+class account_assets(osv.osv):
+    _inherit = 'account.asset.asset'
+    decription = 'Asset whit new fields'
+
+    _columns ={
+    		'ubication':fields.char('Asset Ubication',help="The ubication of this assets"),    
+		'assigned_to':fields.char('Assigned to',help="The asset is assigned"),
+		'date_assigned':fields.date('Date asigned',help="This is the date when was assigned an asset"),
+		'returned':fields.boolean('Returned Asset',help="This is checked when tha asset was returned"),
+	}
